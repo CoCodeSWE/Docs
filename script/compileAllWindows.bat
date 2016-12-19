f515ac758f113ae587a9b93fd88804c46e883a47
ntintorri <tintorri.nicola@gmail.com>
2016-12-19 13:08:03 +0100

    Aggiunto script
    Aggiunto script di compilazione per Windows.

---
+++ script/compileAllWindows.bat
@@ -0,0 +1,13 @@
+cd ../RR/Interni/NormeDiProgetto
+pdflatex NdP.tex
+cd ../StudioDiFattibilita
+pdflatex StudioDiFattibilita.tex
+cd ../Verbale_I_2016-12-10 
+pdflatex Verbale_I_2016-12-10.tex
+cd ../../Esterni/Glossario
+pdflatex Glossario.tex
+cd ../PianoDiProgetto
+pdflatex PianoDiProgetto.tex
+cd ../Verbale_E_2016-12-17
+pdflatex Verbale_E_2016-12-17.tex
+cd ../../../script
